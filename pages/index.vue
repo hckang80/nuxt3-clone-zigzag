4649861a1eb7c581cfdd82617823fed34b216495
hckang80 <hckang80@gmail.com>
2021-12-16 22:17:18 +0900

    refactor: Enhance with easy syntax

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const foo = ref({})
+
+const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+foo.value = await data.json()
+</script>
+
 <template>
   <div>
     {{ foo }}
   </div>
 </template>
-
-<script lang="ts">
-import { onMounted, ref } from 'vue'
-
-export default {
-  setup () {
-    const foo = ref({})
-
-    onMounted(async () => {
-      const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-      foo.value = data
-    })
-
-    return {
-      foo
-    }
-  }
-}
-</script>
