f90d1ce46fbf3c67e0503528de70b52f74eea7f6
hckang80 <hckang80@gmail.com>
2021-12-16 16:23:14 +0900

    feat: Create my first page

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ foo }}
+  </div>
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from 'vue'
+
+export default {
+  setup () {
+    const foo = ref({})
+
+    onMounted(async () => {
+      const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+      foo.value = data
+    })
+
+    return {
+      foo
+    }
+  }
+}
+</script>
