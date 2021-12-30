f7a6357e2cbfa0914e5b0019ebb12a511fbe2a95
hckang80 <hckang80@gmail.com>
2021-12-30 20:04:38 +0900

    feat: Add store list

--- pages/my-store.vue
+++ pages/my-store.vue
@@ -2,6 +2,31 @@
 const saveMyStores = () => {
   console.log('saveMyStores')
 }
+
+const storeList = [
+  {
+    id: 1,
+    title: 'RaRaNe',
+    follower: 375000,
+    image: 'https://cdn.imweb.me/thumbnail/20211217/82aa128ce1000.jpg',
+    products: [
+      'https://cdn.imweb.me/thumbnail/20211206/98e3a1d0b3c15.jpg',
+      'https://cdn.imweb.me/thumbnail/20211211/99626274c06a9.jpg',
+      'https://cdn.imweb.me/thumbnail/20211124/fce4d812dcd1e.jpg',
+    ]
+  },
+  {
+    id: 2,
+    title: '로라네',
+    follower: 100000,
+    image: 'https://cdn.imweb.me/thumbnail/20211217/82aa128ce1000.jpg',
+    products: [
+      'https://cdn.imweb.me/thumbnail/20211212/de855fd8acefc.jpg',
+      'https://cdn.imweb.me/thumbnail/20210730/ee1dd33ea5789.jpg',
+      'https://cdn.imweb.me/thumbnail/20210801/e3cebc27663fb.jpg',
+    ]
+  }
+]
 </script>
 
 <template>
@@ -18,6 +43,34 @@ const saveMyStores = () => {
     </header>
 
     <form @submit.prevent="saveMyStores">
+      <ul class="store-list">
+        <li
+          v-for="{
+            id,
+            image,
+            title,
+            products
+          } in storeList"
+          :key="id"
+          class="store-list__item"
+        >
+          <ul class="main-products">
+            <li
+              v-for="(src, index) in products"
+              :key="index"
+              class="main-products__item"
+            >
+              <div class="main-products__wrap">
+                <img
+                  :src="src"
+                  alt=""
+                  loading="lazy"
+                >
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
       <div class="head-button">
         <button>시작하기</button>
       </div>
