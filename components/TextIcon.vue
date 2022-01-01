8e710d2530aa8fc042484a2d5fb7eaf2e537d435
hckang80 <hckang80@gmail.com>
2022-01-01 21:26:26 +0900

    feat: Create icon component

---
+++ components/TextIcon.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  interface Props {
+    code: string
+    plain?: boolean
+  }
+
+  const props = defineProps<Props>()
+  const { code } = props
+</script>
+
+<template>
+  <i
+    :data-code="code"
+    :class="[
+      'text-icon',
+      { 'is-plain': plain }
+    ]">
+  </i>
+</template>
+
+<style lang="scss">
+.text-icon {
+  &::before {
+    display: inline-block;
+    content: attr(data-code);
+    font-size: 2rem;
+    line-height: 1;
+    color: var(--color-white);
+  }
+  &.is-plain:before {
+    -webkit-text-stroke: 1px var(--color-white);
+    color: transparent;
+  }
+}
+</style>
