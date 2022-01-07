4b1a99545f787a9abb3d37bd7d4a6a7c04433b9e
hckang80 <hckang80@gmail.com>
2022-01-07 23:06:42 +0900

    feat: Install and Init the swiper

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import type { Swiper as TSwiper } from 'swiper/types'
+import 'swiper/css'
+
+const onSwiper = (swiper: TSwiper) => {
+  console.log(swiper)
+}
+const onSlideChange = () => {
+  console.log('slide change')
+}
+</script>
+
+<template>
+  <div>
+    <swiper
+      @swiper="onSwiper"
+      @slideChange="onSlideChange">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+    </swiper>
+  </div>
+</template>
