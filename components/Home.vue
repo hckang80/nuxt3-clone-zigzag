76add1653b112e1c2d29ec84a53bbdbe096f2e9e
hckang80 <hckang80@gmail.com>
2022-01-16 22:52:33 +0900

    feat: Create mock page

---
+++ components/Home.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import type { Swiper as TSwiper } from 'swiper/types'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import SwiperCore, {
+  Pagination
+} from 'swiper'
+
+SwiperCore.use([Pagination])
+
+const navItems = [
+  {
+    id: 1,
+    title: '홈'
+  },
+  {
+    id: 2,
+    title: 'brand'
+  },
+  {
+    id: 3,
+    title: '베스트'
+  },
+  {
+    id: 4,
+    title: '세일',
+    sup: 'N'
+  },
+]
+
+const pagination = {
+  clickable: true,
+  renderBullet (index: number, className: string) {
+    const { title, sup = '' } = navItems[index]
+    const supEl = sup && `<sup>${sup}<sup>`
+    return `<span class="${className}">${title}${supEl}</span>`
+  }
+}
+
+const onSwiper = (swiper: TSwiper) => {
+  console.log(swiper)
+}
+const onSlideChange = () => {
+  console.log('slide change')
+}
+</script>
+
+<template>
+  <div>
+    <swiper
+      class="main-tabs"
+      :pagination="pagination"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange">
+      <swiper-slide
+        v-for="({ title }) in navItems">
+        {{ title }}
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
