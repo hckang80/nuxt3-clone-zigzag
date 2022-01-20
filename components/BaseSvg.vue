dc51671494b8b1ca02ede30bb681350e3e9fe054
hckang80 <hckang80@gmail.com>
2022-01-20 21:25:02 +0900

    feat: Create the SVG component

---
+++ components/BaseSvg.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface Props {
+  width?: number | string
+  height?: number | string
+  viewBox?: string
+  name?: string
+  color?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  width: 18,
+  height: 18,
+  viewBox: '0 0 24 24',
+  name: '',
+  color: '#fff',
+})
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    :viewBox="viewBox"
+  >
+    <title :id="name" lang="en">{{ name }}</title>
+    <g :fill="color">
+      <slot />
+    </g>
+  </svg>
+</template>
