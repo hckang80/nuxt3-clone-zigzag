6bbf53827646894952207714c533e39120948d67
hckang80 <hckang80@gmail.com>
2022-01-20 21:31:59 +0900

    feat: Inject common header

--- components/Store.vue
+++ components/Store.vue
@@ -1,5 +1,22 @@
 <template>
   <section>
-    <h1>스토어</h1>
+    <header>
+      <div class="util-menu">
+        <h1 class="util-menu__title">
+          스토어
+        </h1>
+        <aside class="util-menu__aside">
+          <button>
+            <base-svg
+              width="20"
+              height="20"
+              color="#ff63ab"
+            >
+              <IconCart />
+            </base-svg>
+          </button>
+        </aside>
+      </div>
+    </header>
   </section>
 </template>
