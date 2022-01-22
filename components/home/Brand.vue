1638ffdc6a25c53fdf245aeb099f6b1d4e44ab9e
hckang80 <hckang80@gmail.com>
2022-01-22 20:04:44 +0900

    feat: Create home components

---
+++ components/home/Brand.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <section>
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+  </section>
+</template>
---
+++ components/home/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <section>
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+    content<br><br>
+  </section>
+</template>
