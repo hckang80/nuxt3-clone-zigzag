0ef2810d7dd76746c05e5a42570402248cb4f669
hckang80 <hckang80@gmail.com>
2022-01-27 00:18:19 +0900

    feat: Remove test files

--- test-utils/global-test-utils-config.ts
+++
@@ -1,8 +0,0 @@
-import { ref } from 'vue'
-
-/** Mock Nuxt's useState to be a simple ref for unit testing. **/
-jest.mock('#app', () => ({
-  useState: <T>(key: string, init: () => T) => {
-    return ref(init())
-  },
-}))
